ab7fb47520a7acb9987ecdc1ea18980c787a5ff4
jonghyun0525 <jyu9513@gmail.com>
2023-07-14 14:40:44 +0900

    swap

--- Git_test/소스.cpp
+++ Git_test/소스.cpp
@@ -1,24 +1,62 @@
 #include <stdio.h>
+#define ABS(x) ((x) < 0) ? -(x) : (x)	// 매크로 함수는 자료 독립적!
 
-#define ABS(val) ((val) < 0) ? - (val) : (val))
-
-int abs(int val);
-float abs(float val);
+void swap_val(int a, int b);
+void swap(int *a, int *b);
 
 int main()
 {
-	int i = -1;
-	float a = 2.0;
-	printf ("i가 %d 일 때 i의 절대값은 %d 입니다\n", i, ABS(-1);
-	printf ("a가 %f 일 때 a의 절대값은 %f 입니다\n", a, ABS(-2.0);
-	return 0;
-}
-int abs(int val)			// argument val의 절대값 반환
-{
-	return (val < 0) ? -val : val;
+	int i = -100;
+	int j = 100;
+	printf("a: %d	b: %d\n", i, j);		// 원본 데이터
+	printf(".... swap processing ....\n");
+	swap(&i, &j);
+	/*swap_val(i, j);*/	// call-by-value
 
+	printf("a: %d	b: %d\n", i, j);
+	//float a = -2.0;
+	//printf("i 가 %d 일때 i의 절대값은 %d 입니다", i, ABS(i));
+	//printf("a 가 %f 일때 a의 절대값은 %f 입니다", a, ABS(a));
 }
-float abs(float val)		// argument val의 절대값 반환
+void swap(int *a, int *b)					// call-by-reference: 주소, 포인터
 {
-	return (val < 0) ? -val : val;
+	int c = *a;
+	printf("	.... In swap function ....\n");
+	printf("	a: %d	b: %d\n", *a, *b);	// 원본 데이터
+	printf("	.... swap processing ....\n");
+	*a = *b; *b = c;
+	printf("	a: %d	b: %d\n", *a, *b);	// 처리 후 데이터
 }
+void swap_val(int a, int b)					// call-by-value
+{
+	int c = a;
+	printf("	.... In swap function ....\n");
+	printf("	a: %d	b: %d\n", a, b);	// 원본 데이터
+	printf("	.... swap processing ....\n");
+	a = b; b = c;
+	printf("	a: %d	b: %d\n", a, b);	// 처리 후 데이터
+}
+
+//int abs(int val);
+//float fabs(float val);
+//
+//int main()
+//{
+//    int i = -1;
+//    float a = -2.0;
+//    printf("i 가 %d 일때 i의 절대값은 %d 입니다", i, abs(i));
+//    printf("a 가 %f 일때 a의 절대값은 %f 입니다", a, fabs(a));
+//}
+//
+//int abs(int val)    // argument val의 절대값 반환
+//{
+//    if (val < 0) return -val;
+//    return val;
+//
+//    /*return (val < 0) ? -val : val;*/
+//}
+//
+//float fabs(float val)
+//{
+//    return (val < 0) ? -val : val;
+//}
